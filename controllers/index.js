23db3a2f93bcab7232e6b4990a36f961d9ef790f
Anthony Potts <anthonypotts@Anthonys-MacBook-Pro-2.local>
2021-01-22 20:45:50 -0600

    completed homepage with handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
+// to implement the view routes to the user with MVC
+const homeRoutes = require('./home-routes.js');
+
+// routes to enable viewing of homepage
+router.use('/', homeRoutes);
 
 router.use('/api', apiRoutes);
 
