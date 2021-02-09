f0e369aed5744459f87aeb7957165c6a4761ca5e
Anthony Potts <anthonypotts@Anthonys-MacBook-Pro-2.local>
2021-02-08 23:54:34 -0600

    fixed login and created upvote

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,14 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
-// to implement the view routes to the user with MVC
+
+const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 
 // routes to enable viewing of homepage
 router.use('/', homeRoutes);
-
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.status(404).end();
-});
+// router.use((req, res) => {
+//   res.status(404).end();
+// });
 
 module.exports = router;
